4966965bbacfaad9fc55213ca85c5ebd6d637f46
major-sd <127623469+major-sd@users.noreply.github.com>
2023-12-12 21:31:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,28 +13,25 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+def find_largest(num1, num2, num3):
+  """
+  This function finds the largest of three numbers.
+  """
+  largest = max(num1, num2, num3)
+  return largest
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+st.title("Find the Largest Number")
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# Input fields for the three numbers
+num1 = st.number_input("Enter the first number:", min_value=-9999999999999, max_value=9999999999999)
+num2 = st.number_input("Enter the second number:", min_value=-9999999999999, max_value=9999999999999)
+num3 = st.number_input("Enter the third number:", min_value=-9999999999999, max_value=9999999999999)
+
+# Find the largest number
+largest_number = find_largest(num1, num2, num3)
+
+# Display the result
+st.write("The largest number is:", largest_number)
+
