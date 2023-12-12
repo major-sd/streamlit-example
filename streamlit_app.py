7f98093f9988a6480fc7749d5df9df6cba716a92
major-sd <127623469+major-sd@users.noreply.github.com>
2023-12-12 21:36:35 +0530

    Update streamlit_app.py
    c2

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,9 @@ def find_largest(num1, num2, num3):
 st.title("Find the Largest Number")
 
 # Input fields for the three numbers
-num1 = st.number_input("Enter the first number:", min_value=-9999999999999, max_value=9999999999999)
-num2 = st.number_input("Enter the second number:", min_value=-9999999999999, max_value=9999999999999)
-num3 = st.number_input("Enter the third number:", min_value=-9999999999999, max_value=9999999999999)
+num1 = st.number_input("Enter the first number:")
+num2 = st.number_input("Enter the second number:")
+num3 = st.number_input("Enter the third number:")
 
 # Find the largest number
 largest_number = find_largest(num1, num2, num3)
