4d7f7751ca26d95d18655ba0b744243200545186
major-sd <127623469+major-sd@users.noreply.github.com>
2023-12-12 21:40:41 +0530

    Update streamlit_app.py
    c5

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,19 +14,7 @@ num1 = st.number_input("Enter the first number:", key="num1", min_value=-9999999
 num2 = st.number_input("Enter the second number:", key="num2", min_value=-9999999999999, max_value=9999999999999, value=0)
 num3 = st.number_input("Enter the third number:", key="num3", min_value=-9999999999999, max_value=9999999999999, value=0)
 
-# Clear input button
-clear_button = st.button("Clear Inputs")
 
-
-# Reset button
-reset_button = st.button("Reset")
-
-if reset_button:
-    # Reset input fields to default values (0)
-    num1 = 0
-    num2 = 0
-    num3 = 0
-    st.experimental_rerun()
 
 # Find the largest number
 largest_number = find_largest(num1, num2, num3)
